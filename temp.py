f15e7a8b50a46917544992fef2369c94d1f307cd
Aishwarya Jayachandran Nair <91624511+aishwariyanair@users.noreply.github.com>
2022-02-18 12:06:35 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -3,11 +3,11 @@ import random
 import time
 
 reddit = praw.Reddit(
-    client_id="iS3mSQTCu7Sd3imSmRPLrg",
-    client_secret="vRsK2BLHUn1UXPZsylRkPs-KIQ5YeA",
+    client_id="",
+    client_secret="",
     user_agent="<console:bot:2.0>",
-    username="Personal-bot1234",
-    password="paru123@"
+    username="",
+    password=" "
 )
 
 
@@ -15,18 +15,20 @@ reddit = praw.Reddit(
 subreddit=reddit.subreddit("television")
 
 quotes=["Never,give up..!!!","Nothing in the world is perement","Live as if this is your last day"]
-for submission in subreddit.hot(limit=10):
-   # print("**************")
-    #print(submission.title)
+for submission in subreddit.hot(limit=5):
+  
+#for client_id,client_secret you'll have to create an account in reddit & got to 'https://www.reddit.com/prefs/apps' & you will find a tab in which 'are you a developer' apppears,click on that and enter details and create app,after that you will get a cliend_id,secret_id 
+
+
 
     for comment in submission.comments:
         if hasattr(comment,"body"):
          comment_lower=comment.body.lower()
-        if "sad" in comment_lower:
+        if "motivation" in comment_lower:
          print("--------")
         print(comment.body)
         random_index=random.randint(0,len(quotes)-1)
         comment.reply(quotes[random_index])
-        time.sleep(660)
+        time.sleep(6)
 
 
